56cd4728b8e18832c39256316e1fce64d0350908
Miguel Cartaxo <miguelfcartaxo@gmail.com>
2015-05-14 15:50:25 +0100

    New stuff

--- src/main/java/pt/tecnico/aasma/AgentMain.java
+++ src/main/java/pt/tecnico/aasma/AgentMain.java
@@ -29,30 +29,67 @@ import cz.cuni.amis.utils.exception.PogamutException;
  */
 public class AgentMain {
     
+    public enum AgentType {
+    REACTIVE , DELIBERATIVE, HYBRID
+    }
     public static void main(String args[]) throws PogamutException {
         
-        new UT2004BotRunner( // class that wrapps logic for bots executions, suitable to run single bot in single JVM
-                ReactiveAgentBlue.class, // which UT2004BotController it should instantiate
-                "ReactiveAgentRed" // what name the runner should be using
-                ).setMain(true) // tells runner that is is executed inside MAIN method, thus it may block the thread and watch whether agent/s are correctly executed
-                .startAgents(new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_BLUE),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_BLUE),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_BLUE),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_BLUE),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_BLUE),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_RED),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_RED),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_RED),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_RED),
-                        new UT2004BotParameters().setAgentId(new AgentId("SPOSH-CTF-Bot-Blue")).setTeam(AgentInfo.TEAM_RED)
-                        );
+        AgentType type = AgentType.REACTIVE;
         
-//        new UT2004BotRunner( // class that wrapps logic for bots executions, suitable to run single bot in single JVM
-//                ReactiveAgentBlue.class, // which UT2004BotController it should instantiate
-//                "ReactiveAgentBlue" // what name the runner should be using
-//                ).setMain(true) // tells runner that is is executed inside MAIN method, thus it may block the thread and watch whether agent/s are correctly executed
-//                .startAgents(4);
-        
+        if(type == AgentType.REACTIVE){
+                new UT2004BotRunner( // class that wrapps logic for bots executions, suitable to run single bot in single JVM
+                        ReactiveAgent.class, // which UT2004BotController it should instantiate
+                        "ReactiveAgentRed" // what name the runner should be using
+                        ).setMain(true) // tells runner that is is executed inside MAIN method, thus it may block the thread and watch whether agent/s are correctly executed
+                        .startAgents(new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED)
+                                );
+              
+        } else if(type == AgentType.DELIBERATIVE){
+              new UT2004BotRunner( // class that wrapps logic for bots executions, suitable to run single bot in single JVM
+                        DeliberativeAgent.class, // which UT2004BotController it should instantiate
+                        "DeliberativeAgent" // what name the runner should be using
+                        ).setMain(true) // tells runner that is is executed inside MAIN method, thus it may block the thread and watch whether agent/s are correctly executed
+                        .startAgents(new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED)
+                                );
+        } else if (type == AgentType.HYBRID){
+               new UT2004BotRunner( // class that wrapps logic for bots executions, suitable to run single bot in single JVM
+                        HybridAgent.class, // which UT2004BotController it should instantiate
+                        "HybridAgent" // what name the runner should be using
+                        ).setMain(true) // tells runner that is is executed inside MAIN method, thus it may block the thread and watch whether agent/s are correctly executed
+                        .startAgents(new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_BLUE),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED),
+                                new UT2004BotParameters().setAgentId(new AgentId("CTF-BOT")).setTeam(AgentInfo.TEAM_RED)
+                                );
+        } else{
+            System.out.println("I DONT HAVE AN AGENT TYPE");
+        }
+              
+
         
         
     }
